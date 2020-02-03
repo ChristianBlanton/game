1c759e6a48eefdf318e42928d0e80ed0aa344e60
Christian <Christian@10.161.116.38>
2020-02-03 10:20:47 -0800

    a

--- 2dgame/src/gameObjects/Sprite.java
+++ 2dgame/src/gameObjects/Sprite.java
@@ -7,8 +7,11 @@ import gameFunctions.Renderer;
 import gfx.ImageTile;
 
 public class Sprite extends GameObject{
+	Random rand= new Random();
 	ImageTile image;
 	private int temp=1;
+	private int dy=1; 
+	private int dx=1;
 	public Sprite(ImageTile image)
 	{
 		this.image=image;
@@ -17,6 +20,7 @@ public class Sprite extends GameObject{
 	
 	public void update(GameContainer gc, float dt) {
 		temp++;
+		
 }
 	
 	public void render(GameContainer gc, Renderer r) {
@@ -24,7 +28,7 @@ public class Sprite extends GameObject{
 			temp=0;
 		r.drawImageTile(image, posX, posY, temp, 0);
 		//r.drawRect(posX, posY, width, height, Pixel.randColor());
-		//r.noiseGen();
+		r.noiseGen();
 		//r.noiseFlicker();
 	}
 }
